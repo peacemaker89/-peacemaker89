5c7a5fb815b5e290615260a2983ef849da5afae1
Dima <myEmail@example.com>
2018-07-16 21:28:58 +0300

    первая html- страница.

---
+++ scripts.js
@@ -0,0 +1,62 @@
+function cube(num){
+    return num * num* num;
+}
+
+function  mine(num1,num2) {
+    if (num1 < num2) {
+        return num1;
+    }else {
+        return num2
+    }
+}
+function  day0fWeek(day) {
+    switch( day ){
+        case 1: return 'пн';
+        case 1: return 'вт';
+        case 1: return 'ср';
+        case 1: return 'чт';
+        case 1: return 'пт';
+        case 1: return 'сб';
+        case 1: return 'вс';
+    }
+    return "Неверный день";
+}
+
+
+var result;
+
+result=cube(4);
+
+alert(day0fWeek(3));
+alert(day0fWeek(5));
+
+
+var user={};
+
+user.name='Вася';
+user.surname='Петров';
+user.name='Сергей';
+
+delete user.name;
+
+
+
+
+
+function  isEmpty(obi){
+    for (war prop in obi)
+    {
+        return false;
+
+
+        return true;
+    }
+
+
+
+    var style= ['Джаз','Блюз']
+console.log(styles);
+
+    styles[styles.length - 2]='Класика'
+
+
