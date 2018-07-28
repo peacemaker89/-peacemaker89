58a145c4a33bb3cc26fdd4986fb1285da18863a8
Olga <peacemaker.water23@gmail.com>
2018-07-28 22:44:11 +0300

    javascript1

---
+++ homework5.js
@@ -0,0 +1,30 @@
+var c = 15;/*переменная с*/
+var d = 2;/*переменная d*/
+var result = c + d;/*сумма переменных c и d присвоина переменной result*/
+alert (result);/*значение переменной result на экране*/
+
+var a=10;
+var b=2;
+var c=5;
+alert (a+b+c);
+
+var a = 17;
+var b = 10;
+var c = a-b;
+var d = 7;
+
+var result = c+d;
+alert(result);
+
+var str = 'Привет Мир!';
+alert(str);
+
+var str1 = 'Привет!';
+var str2 = 'Мир!';
+alert(str1+str2);
+
+var name = 'Оля!';
+alert('Привет,'+name);
+
+var age = 29;
+alert('Мне '+ age +'лет!');
